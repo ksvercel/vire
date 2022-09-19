9e89561ed3081a22438037d12e340321ebda98fd
ksvercel <113374693+ksvercel@users.noreply.github.com>
2022-09-19 15:36:04 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     images: {
         //enter the domain or subdomain where you have WordPress installed 
-      domains: ['verks.xyz'],
+      domains: ['vilals-bb417c.ingress-florina.ewp.live'],
     },
 }
